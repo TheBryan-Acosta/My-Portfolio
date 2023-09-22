c1155d950326cfc45488134c8c670645a933b307
Bryan A <bryanacostadelacruz@gmail.com>
2023-09-21 21:28:53 -0500

    more asthetic

--- src/components/Quote/index.js
+++ src/components/Quote/index.js
@@ -1,24 +1,21 @@
 import React from "react";
 import { Col, Container, Row } from "react-bootstrap";
 import "./index.css";
- import moon from "../../assets/projects/moon.png";
+import moon from "../../assets/projects/pngegg.png";
 const Quote = () => {
 	return (
 		<Container className="about-container">
 			<Row>
 				<Col>
-					<img
-						src={moon}
-						alt="Moon"
-						className="about-img"
-					></img>
+					<img src={moon} alt="Moon" className="about-img"></img>
 				</Col>
 				<Col>
 					<p className="about-text">
-						Aim for the Moon. If you miss, you may hit a star...
+						The marble not yet carved can hold the form of every thought the
+						greatest artist has.
 					</p>
 					<br />
-					<p className="about-text-name">-W. Clement Stone</p>
+					<p className="about-text-name">-Michelangelo</p>
 				</Col>
 			</Row>
 		</Container>
